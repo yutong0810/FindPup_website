a05a38adc0b7e1386dc985c785858fdf89cf7c07
ZiyiZ-cloud <82247271+ZiyiZ-cloud@users.noreply.github.com>
2022-08-21 15:11:18 -0400

    set up

---
+++ backend/config.js
@@ -0,0 +1,34 @@
+"use strict";
+
+/** Shared config for application; can be required many places. */
+
+require("dotenv").config();
+require("colors");
+
+const SECRET_KEY = process.env.SECRET_KEY || "secret-dev";
+
+const PORT = process.env.PORT || 3001;
+
+// Use dev database, testing database, or via env var, production database
+function getDatabaseUri() {
+  return  process.env.DATABASE_URL || "findpup";
+}
+
+// Speed up bcrypt during tests, since the algorithm safety isn't being tested
+//
+// WJB: Evaluate in 2021 if this should be increased to 13 for non-test use
+const BCRYPT_WORK_FACTOR = 12;
+
+console.log("findpup Config:".green);
+console.log("SECRET_KEY:".yellow, SECRET_KEY);
+console.log("PORT:".yellow, PORT.toString());
+console.log("BCRYPT_WORK_FACTOR".yellow, BCRYPT_WORK_FACTOR);
+console.log("Database:".yellow, getDatabaseUri());
+console.log("---");
+
+module.exports = {
+  SECRET_KEY,
+  PORT,
+  BCRYPT_WORK_FACTOR,
+  getDatabaseUri,
+};
